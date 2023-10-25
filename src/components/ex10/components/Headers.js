8b94bf243bb77b07daa26f6735e4e4bfbbbc6a56
unknown <carla7039@naver.com>
2023-10-25 17:53:39 +0900

    music app menu

--- src/components/ex10/components/Headers.js
+++ src/components/ex10/components/Headers.js
@@ -4,6 +4,7 @@ import {
   faCaretDown,
   faForward,
   faForwardStep,
+  faLeftLong,
   faPause,
   faRepeat,
   faShuffle,
@@ -11,6 +12,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { colors } from "../../../GlobalStyled";
+import { useState } from "react";
 
 const SHeader = styled.header`
   display: flex;
@@ -36,18 +38,59 @@ const MenuBtn = styled.div`
   align-items: center;
   border: 1px solid #fff;
   border-radius: 50%;
+  position: relative;
+  z-index: 10;
   cursor: pointer;
 `;
 
+const MenuWrap = styled.ul`
+  position: absolute;
+  top: 0;
+  left: ${(props) => props.$isActive};
+  width: 100%;
+  height: 100%;
+  background-color: lightgray;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 60% 0;
+  font-size: 30px;
+  font-weight: 600;
+  color: ${colors.darkGray};
+  transition: 0.5s;
+`;
+
 export const Headers = () => {
+  // const [num, setNum] = useState(true);
+  const [leftResult, setLeftResult] = useState("100%");
+  const onClickMenu = () => {
+    // if (num === 0) {
+    //   setLeftResult("100%");
+    //   setNum(num + 1);
+    // } else if (num === 1) {
+    //   setLeftResult("0");
+    //   setNum(num - 1);
+    // }
+
+    // num === true ? setLeftResult(0) : setLeftResult("100%");
+    leftResult === "100%" ? setLeftResult(0) : setLeftResult("100%");
+  };
   return (
     <SHeader>
       <MoreBtn>
         <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
       </MoreBtn>
-      <MenuBtn>
+      <MenuBtn onClick={onClickMenu}>
         <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
       </MenuBtn>
+
+      <MenuWrap $isActive={leftResult}>
+        <li>MENU</li>
+        <li>MENU</li>
+        <li>MENU</li>
+        <li>MENU</li>
+      </MenuWrap>
     </SHeader>
   );
 };
